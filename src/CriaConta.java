e6eae0e6d299646ff798aea6933414d110bfb251
Clayton Zambon <zambon@outlook.com.br>
2024-10-22 16:16:24 -0300

    FEAT: Criando objetos parte 2

--- src/CriaConta.java
+++ src/CriaConta.java
@@ -4,10 +4,14 @@ public class CriaConta {
 	public static void main(String[] args) {
 		Conta primeiraConta = new Conta();
 		primeiraConta.saldo = 200.00;
-		System.out.println(primeiraConta.saldo);
+		System.out.println("Conta 1 - Saldo inicial: " + primeiraConta.saldo);
 		
 		primeiraConta.saldo += 100.00;
-		System.out.println(primeiraConta.saldo);
+		System.out.println("Conta 1 - Saldo atualizado: " + primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50.00;
+		System.out.println("Conta 2 - Saldo inicial: " + segundaConta.saldo);
 	}
 
 }
