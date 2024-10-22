9a057584c339b7cb0b423fd58acf151f110f621e
Clayton Zambon <zambon@outlook.com.br>
2024-10-22 16:25:00 -0300

    FEAT: Teste referencias parte 03

--- src/TesteReferencias.java
+++ src/TesteReferencias.java
@@ -9,6 +9,17 @@ public class TesteReferencias {
 
 		Conta segundaConta = primeiraConta;
 		System.out.println("Conta 2 - Saldo inicial: " + segundaConta.saldo);
+		
+		segundaConta.saldo += 100;
+		System.out.println("Conta 2 - Saldo atualizado: " + segundaConta.saldo);
+		System.out.println("Conta 1 - Saldo atualizado: " + primeiraConta.saldo);
+		
+		if (primeiraConta == segundaConta) {
+			System.out.println("É a mesma conta");
+		}
+		
+		System.out.println(primeiraConta);
+		System.out.println(segundaConta);
 	}
 
 }
