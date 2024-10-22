74078256c825d9016686cd7480d6ff43f1323473
Clayton Zambon <zambon@outlook.com.br>
2024-10-22 16:39:26 -0300

    FEAT: Cria  metodos - parte 03

--- src/Conta.java
+++ src/Conta.java
@@ -6,15 +6,26 @@ public class Conta {
 	int numero;
 	String titular;
 	
-	void deposita(double valor) {
+	public boolean deposita(double valor) {
 		if (valor > 0.0) {
 			this.saldo = this.saldo + valor;
+			return true;
 		}
+		return false;
 	}
 	
 	public boolean saca(double valor) {
 		if (this.saldo >= valor) {
 			this.saldo = this.saldo - valor;
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		if (saldo >= valor) {
+			this.saldo = this.saldo - valor;
+			destino.deposita(valor);
 			return true;
 		}
 		return false;
--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -1,13 +1,33 @@
 
 public class TestaMetodo {
 	public static void main(String[] args) {
-		Conta contaExemplo = new Conta();
-		contaExemplo.saldo = 100;
-		contaExemplo.deposita(50);
-		System.out.println("Conta exemplo - Saldo atualizado: " + contaExemplo.saldo);
+//		Conta contaExemplo = new Conta();
+//		contaExemplo.saldo = 100;
+//		contaExemplo.deposita(50);
+//		System.out.println("Conta exemplo - Saldo atualizado: " + contaExemplo.saldo);
+//		
+//		boolean conseguiuSacar = contaExemplo.saca(20);
+//		System.out.println("Conta exemplo - Saque realizado? " + conseguiuSacar);
+//		System.out.println("Conta exemplo - Saldo atualizado: " + contaExemplo.saldo);
+		Conta contaVenus = new Conta();
+		contaVenus.saldo = 100;
+		System.out.println("Conta Venus - Saldo: R$ " + contaVenus.saldo);
 		
-		boolean conseguiuSacar = contaExemplo.saca(20);
-		System.out.println("Conta exemplo - Saque realizado? " + conseguiuSacar);
-		System.out.println("Conta exemplo - Saldo atualizado: " + contaExemplo.saldo);
+		boolean conseguiuDepositar = contaVenus.deposita(50);
+		System.out.println("\nConta Venus - Depositou? " + conseguiuDepositar);
+		System.out.println("Conta Venus - Saldo: R$ " + contaVenus.saldo);
+		
+		boolean conseguiuSacar = contaVenus.saca(20);
+		System.out.println("\nConta Venus - Sacou? " + conseguiuSacar);
+		System.out.println("Conta Venus - Saldo: R$ " + contaVenus.saldo);
+		
+		Conta contaMarte = new Conta();
+		contaMarte.deposita(1000);
+		System.out.println("\nConta Marte - Saldo: R$ " + contaMarte.saldo);
+		
+		boolean conseguiuTransferir = contaMarte.transfere(300, contaVenus);
+		System.out.println("\nConta Marte - Transferiu? " + conseguiuTransferir);
+		System.out.println("Conta Marte - Saldo: R$ " + contaMarte.saldo);
+
 	}
 }
